0ff65af8d2728637d3a8f768b18cae9787404fe3
bnuchen <bnuchengg@gmail.com>
2024-03-29 08:16:03 +0800

    fix

--- src/main/java/com/cg/restdemo/controller/HomeController.java
+++ src/main/java/com/cg/restdemo/controller/HomeController.java
@@ -3,7 +3,9 @@ package com.cg.restdemo.controller;
 import com.cg.restdemo.service.FoodTruckService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -16,7 +18,12 @@ public class HomeController {
   @Autowired
   private FoodTruckService foodTruckService;
 
-  @RequestMapping("/applicant/{applicant}")
+  @GetMapping("")
+  public String index(@PathVariable("applicant") String applicant) throws ExecutionException {
+    return "index";
+  }
+
+  @PostMapping("/applicant/{applicant}")
   @ResponseBody
   public Object getByApplicant(@PathVariable("applicant") String applicant) throws ExecutionException {
     return foodTruckService.getByApplicant(applicant);
